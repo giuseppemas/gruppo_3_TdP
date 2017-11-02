c767be4ae19015c4af7e3904288891caea604c67
BonnieCharlie <BonnieCharlie@users.noreply.github.com>
2017-11-02 15:17:25 +0100

    Vera copia

--- 3gruppo_2_TdP/pkg_2/MyTreeMap.py
+++ 3gruppo_2_TdP/pkg_2/MyTreeMap.py
@@ -1,9 +1,9 @@
-from TdP_collections.tree import tree
+from TdP_collections.map.binary_search_tree import TreeMap
 
 '''
 Progettare la classe MyTreeMap che espone la stessa interfaccia di TreeMap ma implementa i metodi after() e before() con
 complessità di tempo O(1), senza aumentare la complessità di tempo di tutti gli altri metodi.
 '''
 
-class MyTreeMap(tree):
+class MyTreeMap(TreeMap):
     pass
