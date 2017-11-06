9bc1bf9a40bc773cb254cc95b9dff0e3db8b6484
Alfonso Esposito <32770446+EspositoAlfonso@users.noreply.github.com>
2017-11-06 11:22:48 +0100

    Bho

--- 3gruppo_2_TdP/pkg_1/prova_sperimentale_alfonso.py
+++ 3gruppo_2_TdP/pkg_1/prova_sperimentale_alfonso.py
@@ -1,3 +1,8 @@
 from TdP_collections.map.avl_tree import AVLTreeMap
 from TdP_collections.map.red_black_tree import RedBlackTreeMap
 
+print("Conteggio Ricerche: ")
+rb = RedBlackTreeMap([1,2,3,4,5])
+avl = AVLTreeMap([1,2,3,4,5])
+
+
